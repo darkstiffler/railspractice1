2d8653991ce636e8a0891a28e2e3c20b711e93c3
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-27 17:11:26 -0700

    add general controllers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railspractice1::Application.config.secret_key_base = 'dc164a697906bfda96342624abc036359f2c79b9ec8ca0c70c8e927dafc136f329b66310ae6c6a86ebc93d7ebfac30473f9c3ae6d8c9be585f708a922183938c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railspractice1::Application.config.session_store :cookie_store, key: '_railspractice1_session'
